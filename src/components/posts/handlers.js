2306e5633e057036caf262ba8cfda06a9235e4a6
Jesús Urrutia <jesus.urrutia@gmail.com>
2020-04-23 22:28:38 -0400

    feat: loggin with console.log

--- src/components/posts/handlers.js
+++ src/components/posts/handlers.js
@@ -1,11 +1,25 @@
+/* eslint-disable no-console */
 const posts = {};
 
 function get(req, res) {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  const post = posts[id];
+    const post = posts[id];
 
-  res.json({ post });
+    if (!post) {
+      console.log(`Post with id ${id} was not found`);
+
+      res.status(404).json({ error: 'Post not found' });
+      return;
+    }
+
+    res.json({ post });
+  } catch (e) {
+    console.log(e);
+
+    res.status(500).json({ error: 'Something went wrong' });
+  }
 }
 
 export {
